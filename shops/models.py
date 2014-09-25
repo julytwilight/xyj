1339ff9f944841e12a9c0f443a9ce05722653a6c
Mikasa <mikasayee@neatpath.com>
2014-09-25 16:58:56 +0800

    production model

--- shops/models.py
+++ shops/models.py
@@ -6,4 +6,4 @@ from django.utils.translation import ugettext_lazy as _
 
 class Production(models.Model):
     name = models.CharField(_('production name'), max_length=60, unique=True)
-    
+    price = models.DecimalField(_('production price'), decimal_places=2)  #  max_digits=5
