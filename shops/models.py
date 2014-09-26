6594b7801b61c86809c033099840b8eb9ae2281e
Mikasa <mikasayee@neatpath.com>
2014-09-26 18:03:58 +0800

    shop models

--- shops/models.py
+++ shops/models.py
@@ -1,9 +1,14 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
 
+from ahead.utils.db import DateTimeModel
 # Create your models here.
 
 
-class Production(models.Model):
-    name = models.CharField(_('production name'), max_length=60, unique=True)
-    price = models.DecimalField(_('production price'), decimal_places=2)  #  max_digits=5
+class Production(models.Model, DateTimeModel):
+    name         = models.CharField("产品名称", max_length=60, unique=True)
+    price        = models.DecimalField("价格", max_digits=5, decimal_places=2)
+    ingredients  = models.CharField("原料", max_length=255, blank=True)
+    guarantee    = models.PositiveSmallIntegerField("保质期", default=6)
+    introduction = models.TextField("介绍", blank=True)
+    pic1         = models.ImageField(upload_to="productions")
+    pic2         = models.ImageField()
